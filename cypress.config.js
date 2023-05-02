d4658b501e6df104d78d66ca14f0c09632a750e5
rmhyuen <robertmhyuen@gmail.com>
2023-05-02 11:29:54 -0700

    Add the first test file

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   e2e: {
+    baseUrl: "http://localhost:3000",
+    supportFile: false,
+    fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
