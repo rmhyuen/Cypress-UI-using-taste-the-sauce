5b83a9fdbc95ad64add0009e3b3c75099a8e3647
rmhyuen <robertmhyuen@gmail.com>
2023-05-02 13:42:34 -0700

    lesson 2

---
+++ cypress/e2e/lessons/2-confirm-lowest-price.cy.js
@@ -0,0 +1,33 @@
+import "cypress-map"
+
+describe('template spec', () => {
+  context('', () => {
+    it('should login', () => {
+      cy.visit('/')
+      cy.get("[data-test='username']")
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'Username')
+        .type('standard_user')
+      cy.get("[data-test='password']")
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'Password')
+        .type('secret_sauce')
+      cy.get("[data-test='login-button']")
+        .should('be.visible')
+        .click()
+      cy.location('pathname')
+        .should('equal', '/inventory.html')
+      cy.get('.inventory_item_price')
+        .then(console.log) // old way to log, using the console
+        .should("be.visible")
+        .map("innerText") // newer way to map using cypress-map so it's chainable and retries
+        .print()
+        .mapInvoke("slice", 1) //remove the $
+        .print()
+        .map(Number) // map it as Number from string
+        .apply(Cypress._.min) // use lodash to get the min value
+        .print()
+        .should("equal", 7.99)
+    })
+  })
+})
