7644e4461cf022867fd1e897ed40d82df3891240
rmhyuen <robertmhyuen@gmail.com>
2023-05-02 19:33:56 -0700

    lesson 7

---
+++ cypress/e2e/lessons/7-confirm-item-text-details.cy.js
@@ -0,0 +1,42 @@
+// enables typescript checking in javascript files
+// @ts-check
+
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+describe('item', () => {
+  beforeEach(() => {
+    //login
+    cy.visit('/')
+      cy.get("[data-test='username']")
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'Username')
+        .type('standard_user')
+      cy.get("[data-test='password']")
+        .should('be.visible')
+        .and('have.attr', 'placeholder', 'Password')
+        .type('secret_sauce')
+      cy.get("[data-test='login-button']")
+        .should('be.visible')
+        .click()
+      cy.location('pathname')
+        .should('equal', '/inventory.html')
+  })
+
+  context('item details', () => {
+    it('should verify item text details', () => {
+      // confirm there is an item in the inventory
+      // with:
+      //   name: "Sauce Labs Bike Light"
+      //   description: "A red light isn't the desired state in testing but it sure helps when riding your bike at night"
+      //   price: $9.99
+      // https://on.cypress.io/contains
+      // https://on.cypress.io/within
+
+      cy.contains(".inventory_item_name", "Sauce Labs Bike Light")
+      cy.contains(".inventory_item_desc", "A red light isn't the desired state in testing but it sure helps when riding your bike at night")
+      cy.contains(".inventory_item_price", "$9.99")
+    })
+  })
+})
